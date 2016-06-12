ca663fff632334321355ead05cf00bf02c41aeab
Scott <me@chengfeilong.com>
2016-06-12 11:04:18 +0800

    fix back

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+
+    // 签名
     console.log('%c\n'+
                 '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n'+
                 '@@@@@@@@@@@@@@@*.@.     @@@@@@@@@@@@@@@@@@@@@@@@@@@@**@@@@@@@*@@@@@@@@@@@@@@\n'+
@@ -12,6 +14,8 @@ $(function(){
                 '@@@@@@@@@@@@@*     *. .@@@@@*.   .*@@@@. .@.*@@@@@@ .*@@@@@..*@@@@@@@@@@@@@@\n'+
                 '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n',
                 'color:#ff3355');
+
+    // 播放器
     setTimeout(function(){
         if($('#musicContainer').css('display') !== "none"){
             if(!$('#musicContent').find('iframe').length){
@@ -25,8 +29,19 @@ $(function(){
             $('#musicContent').append('<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=450 src="http://music.163.com/outchain/player?type=0&id=317921676&auto=0&height=430"></iframe>');
         }
     });
+
+    // 图片延迟加载
     $("img.lazy").unveil(1000);
     $('.post-listing').scroll(function(){
         $(window).trigger('scroll');
+    });
+
+    // 返回按钮
+    $('a','.back-home').on('click',function(){
+        if(window.history.length > 1){
+            window.history.back();
+        }else{
+            window.location.href = 'http://www.chengfeilong.com';
+        }
     });
 });
